37d6e4f9e01e59393e3af6a1e226eea7d4e978ed
Chauncey2 <gaochangju95@163.com>
2020-06-14 17:15:30 +0800

    keras实践---reuters数据集多分类问题

--- test/retuers_test.py
+++ test/retuers_test.py
@@ -2,19 +2,19 @@ from keras.datasets import reuters
 from keras.utils.np_utils import to_categorical
 from keras import models
 from keras import layers
-
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def vectorize_sequences(sequences,dimension=10000):
     ""
     # 创建一个shape为（len(sequece),dimesion）的0矩阵
-    results = np.zeros((len(sequences),dimesion),dtype='float32') 
+    results = np.zeros((len(sequences),dimension),dtype='float32') 
     for i ,sequences in enumerate(sequences):
         results[i,sequences] = 1.
     return results
 
-def to_one_hot(labels,dimesion=46)；
+def to_one_hot(labels,dimesion=46):
     results = np.zeros((len(labels),dimesion),dtype='float32') 
     for i ,sequences in enumerate(labels):
         results[i,sequences] = 1.
@@ -27,19 +27,21 @@ def train_model():
     # 编码数据
     x_train = vectorize_sequences(train_data)
     x_test = vectorize_sequences(test_data)
-    one_hot_train_labels = to_categorical(train_data)
+
+    one_hot_train_labels = to_categorical(train_lable)
     one_hot_test_labels = to_categorical(test_labels)
 
     # 定义model
     model = models.Sequential()
     model.add(layers.Dense(64,activation='relu',input_shape=(10000,)))
-    model.add(layers.Dense(64,activation='relu'))
+    model.add(layers.Dense(128,activation='relu'))
+    model.add(layers.Dense(128,activation='relu')) # z再添加一个隐藏层
     model.add(layers.Dense(46,activation='softmax'))
 
     # 编译model
     model.compile(optimizer='rmsprop',
                   loss = 'categorical_crossentropy',
-                  metrics='accuracy'
+                  metrics=['accuracy']
                  )
 
     # 留出验证集
@@ -55,8 +57,9 @@ def train_model():
                         epochs=20,
                         batch_size=512,
                         validation_data=(x_val,y_val))
+    result = model.predict(x_test)
 
-    return history
+    return history,result
 
 def show_loss(history):
     history_dict = history.history
@@ -76,15 +79,19 @@ def show_loss(history):
 
 def show_accuracy(history):
     history_dict = history.history
-    acc = history_dict['acc']
-    val_acc = history_dict['val_acc']
-    val_loss_values = history_dict['val_loss']
+
+    
+    # for item in history_dict.items():
+    #     print(item)
+    acc = history_dict['accuracy']
+    val_acc = history_dict['val_accuracy']
+    # val_loss_values = history_dict['val_loss']
 
     epochs = range(1,len(val_acc)+1)
 
     plt.plot(epochs,acc,'bo',label='Trainning acc')
     plt.plot(epochs,val_acc,'b',label='Validation acc')
-    plt.title("Trainning and validation loss")
+    plt.title("Trainning and validation accuracy")
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
@@ -95,3 +102,5 @@ def show_accuracy(history):
 if __name__ == '__main__':
     history,result = train_model()
     print(result)
+    show_accuracy(history)
+    # show_loss(history)
